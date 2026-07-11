68cf27252db22f1a619adcb1a0886bd0d102db32
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-07-11 11:10:17 +0300

    Update AppRouter to provide QuoteCubit and dependencies to HomeScreen.

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,12 +1,22 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quotely/core/constants/app_constants.dart';
+import 'package:quotely/features/home/data/repo/home_repo.dart';
+import 'package:quotely/features/home/data/webservices/webservices.dart';
+import 'package:quotely/features/home/logic/quote_cubit.dart';
 import 'package:quotely/features/home/ui/home_screen.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings setting) {
     switch (setting.name) {
       case AppConstants.homeScreen:
-        return MaterialPageRoute(builder: (_) => const HomeScreen());
+        return MaterialPageRoute(builder: (_) =>
+            BlocProvider(
+              create: (context) =>
+                  QuoteCubit(HomeRepo(webServices: Webservices(Dio()))),
+              child: HomeScreen(),
+            ));
       default:
         return MaterialPageRoute(builder: (_) => const HomeScreen());
     }
