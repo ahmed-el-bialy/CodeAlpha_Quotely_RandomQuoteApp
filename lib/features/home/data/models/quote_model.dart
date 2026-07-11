a9d592b55033ef3f8653711e08af0bb8a8b6c7f1
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-07-11 10:41:33 +0300

    Add QuoteModel with JSON serialization support.

---
+++ lib/features/home/data/models/quote_model.dart
@@ -0,0 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'quote_model.g.dart';
+
+@JsonSerializable()
+class QuoteModel {
+  @JsonKey(name: "quote")
+  final String quote;
+
+  @JsonKey(name: "author")
+  final String author;
+
+  QuoteModel({required this.quote, required this.author});
+
+  factory QuoteModel.fromJson(Map<String, dynamic> json) =>
+      _$QuoteModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$QuoteModelToJson(this);
+}
---
+++ lib/features/home/data/models/quote_model.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'quote_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+QuoteModel _$QuoteModelFromJson(Map<String, dynamic> json) => QuoteModel(
+  quote: json['quote'] as String,
+  author: json['author'] as String,
+);
+
+Map<String, dynamic> _$QuoteModelToJson(QuoteModel instance) =>
+    <String, dynamic>{'quote': instance.quote, 'author': instance.author};
