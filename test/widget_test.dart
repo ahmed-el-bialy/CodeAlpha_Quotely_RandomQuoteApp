7dab25e0bfca2bc0e47dd8d1d4e187b7f7991786
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-07-11 09:35:32 +0300

    fix AppRouter import path.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:quotely/core/routiong/app_router.dart';
+import 'package:quotely/core/routing/app_router.dart';
 import 'package:quotely/main.dart';
 
 void main() {
